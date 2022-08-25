f4c924c56859e75cecfc004479fbfb9597f425ce
akitomato <tobewxjp24x@gmail.com>
2022-08-26 01:08:31 +0900

    生き物検索機能実装

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,33 @@
 class AnimalsController < ApplicationController
+  def index
+    @animals =Animal.search(params[:keyword])
+  end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+    @animal.save
+    redirect_to root_path
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def search
+  end
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(
+      :name, :detail, :category_id, :image
+    ).merge(aquarium_id: params[:aquarium_id])
+  end
+
 end
--- app/models/animal.rb
+++ app/models/animal.rb
@@ -4,5 +4,13 @@ class Animal < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
 
+  def self.search(search)
+    if search != ""
+      Animal.where('text LIKE(?)', "%#{search}%")
+    else
+      Animal.all
+    end
+  end
+
   validates :genre_id, numericality: { other_than: 1 , message: "は選択必須です"}
 end
